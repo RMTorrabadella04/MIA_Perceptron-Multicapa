5ca3a4dd44a9dda422117525dc606cd407bb6557
RMTorrabadella04 <raul.martin.torrabadella@gmail.com>
2026-02-11 17:15:39 +0100

    Subí requirement.txt

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from mia-perceptron-multicapa!")
+# Con Sklean pillare el dataset EMNIST, Normalizare, Split, Confusion Matrix, Classification Report
+# No pillare nada para hacer el modelo, el modelo lo hare a mano con numpy solo.
+
+from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler as minmax
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Cargamos el dataset EMNIST
+
+datos = datasets.fetch_openml('emnist_balanced', version=1, as_frame=False)
+X = datos.data
+y = datos.target
+
+# Normalizamos
+
+scaler = minmax()
+X_normalizado = scaler.fit_transform(X)
+
+# Dividimos en train y test
+
+X_train, X_test, y_train, y_test = train_test_split(X_normalizado, y, test_size=0.2, random_state=42)
 
 
-if __name__ == "__main__":
-    main()
